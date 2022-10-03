8af47b80c2abca599d9a83063363da657334e938
Akanksha Gupta <ag6389936@gmail.com>
2022-10-04 01:01:12 +0530

    add a file.

---
+++ function.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+
+int solveMeFirst(int a, int b) ;
+int main()
+{
+int a,b,c;
+// Hint: Type return a+b; below
+scanf("%d %d",&a,&b);
+int x=solveMeFirst(a,b);
+printf("%d\n",x);
+printf("%d",c);
+// return c;
+}
+
+ 
+int solveMeFirst(int a,int b)
+{
+int c;
+c=a+b;
+// return c;
+}
+    
